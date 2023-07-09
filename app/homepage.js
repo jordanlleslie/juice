82f816258bb2a0fc7e907cda38ceb55475d80744
Jordan Leslie <jordan02leslie@gmail.com>
2023-07-09 10:14:43 +0800

    changed main title to svg

--- app/homepage.js
+++ app/homepage.js
@@ -1,6 +1,5 @@
-"use client";
-
 import styles from "../styles/styles.module.css";
+import Image from "next/image";
 
 export const metadata = {
   title: "Emory Juice Ultimate",
@@ -9,7 +8,13 @@ export const metadata = {
 export default function HomePage() {
   return (
     <>
-      <p className={styles.mainTitle}>EMORY JUICE</p>
+      <Image
+        src="/images/MainTitleHorizontal.svg"
+        width={1}
+        height={1}
+        alt="Emory Juice Main Title"
+        style={{ width: "100%", height: "auto" }}
+      />
       <p>Home Page Coming Soon!</p>
     </>
   );
