f795d3a79fe76b6e96f1b2a61410a5ba342e50bf
Jordan Leslie <jordan02leslie@gmail.com>
2023-07-09 13:46:50 +0800

    some work on styling

--- app/home/socials.js
+++ app/home/socials.js
@@ -1,13 +1,12 @@
 import Image from "next/image";
 import styles from "../../styles/styles.module.css";
 
-const iconSize = 48;
+const iconSize = 32;
 
-const SocialLink = ({ href, handle, fileName }) => {
+const SocialLink = ({ href, fileName }) => {
   return (
-    <a href={href} target="_blank">
+    <a href={href} target="_blank" className={styles.socialsIcon}>
       <Image src={"images/" + fileName} width={iconSize} height={iconSize} />
-      {handle}
     </a>
   );
 };
@@ -17,19 +16,13 @@ export default function Socials() {
     <div className={styles.socialsContainer}>
       <SocialLink
         href="https://www.instagram.com/emoryjuiceultimate/"
-        handle="@EmoryJuiceUltimate"
         fileName="Instagram.svg"
       />
       <SocialLink
         href="https://twitter.com/EmoryJuice"
-        handle="@EmoryJuice"
         fileName="Twitter.svg"
       />
-      <SocialLink
-        href="mailto: emoryultimate@gmail.com"
-        handle="emoryultimate@gmail.com"
-        fileName="Email.svg"
-      />
+      <SocialLink href="mailto: emoryultimate@gmail.com" fileName="Email.svg" />
     </div>
   );
 }
