350dad372688fbcc2041fe3627f52da16034da57
Jordan Leslie <jordan02leslie@gmail.com>
2023-07-31 11:14:21 +0800

    refactor

---
+++ app/shared/Modal.js
@@ -0,0 +1,40 @@
+import { useCallback, useRef, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import styles from "../../styles/styles.module.css";
+
+export function Modal({ children }) {
+  const overlay = useRef(null);
+  const wrapper = useRef(null);
+  const router = useRouter();
+
+  const onDismiss = useCallback(() => {
+    router.back();
+  }, [router]);
+
+  const onClick = useCallback(
+    (e) => {
+      if (e.target === overlay.current || e.target === wrapper.current) {
+        if (onDismiss) onDismiss();
+      }
+    },
+    [onDismiss, overlay, wrapper]
+  );
+
+  const onKeyDown = useCallback(
+    (e) => {
+      if (e.key === "Escape") onDismiss();
+    },
+    [onDismiss]
+  );
+
+  useEffect(() => {
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, [onKeyDown]);
+
+  return (
+    <div ref={overlay} className={styles.modal} onClick={onClick}>
+      {children}
+    </div>
+  );
+}
--- app/watch/VideoModal.js
+++ app/watch/VideoModal.js
@@ -1,6 +1,5 @@
 import styles from "../../styles/styles.module.css";
-import { useCallback, useRef, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { Modal } from "../shared/Modal";
 
 export default function VideoModal() {
   return (
@@ -13,45 +12,8 @@ export default function VideoModal() {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
         className={styles.video}
         loading="eager"
-        frameBorder="0"
+        style={{ border: "none" }}
       ></iframe>
     </Modal>
-  );
-}
-
-export function Modal({ children }) {
-  const overlay = useRef(null);
-  const wrapper = useRef(null);
-  const router = useRouter();
-
-  const onDismiss = useCallback(() => {
-    router.back();
-  }, [router]);
-
-  const onClick = useCallback(
-    (e) => {
-      if (e.target === overlay.current || e.target === wrapper.current) {
-        if (onDismiss) onDismiss();
-      }
-    },
-    [onDismiss, overlay, wrapper]
-  );
-
-  const onKeyDown = useCallback(
-    (e) => {
-      if (e.key === "Escape") onDismiss();
-    },
-    [onDismiss]
-  );
-
-  useEffect(() => {
-    document.addEventListener("keydown", onKeyDown);
-    return () => document.removeEventListener("keydown", onKeyDown);
-  }, [onKeyDown]);
-
-  return (
-    <div ref={overlay} className={styles.modal} onClick={onClick}>
-      {children}
-    </div>
   );
 }
