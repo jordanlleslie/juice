f5aa7a885b16aae5b250bafc7641d723d2ace447
Jordan Leslie <jordan02leslie@gmail.com>
2023-08-04 09:47:55 +0800

    cleanup

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
 import Layout from "./layout/Layout";
 import "../styles/styles.module.css";
 
-export default function RootLayout({
-  // Layouts must accept a children prop.
-  // This will be populated with nested layouts or pages
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
